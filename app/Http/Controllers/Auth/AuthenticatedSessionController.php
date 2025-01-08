796bd62b647d8a5bcb2558e7faed6c4b634296a0
Riddhi Gohil <20131391+riddhigohil1@users.noreply.github.com>
2025-01-08 09:48:57 -0500

    Install Filament and Create Admin Panel

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use App\RolesEnum;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
+use Spatie\Permission\Contracts\Role;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -27,13 +30,25 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(LoginRequest $request): \Symfony\Component\HttpFoundation\Response
     {
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        /**
+         * @var User $user
+         */
+        $user = Auth::user();
+        $route = '/';
+        if($user->hasAnyRole([RolesEnum::Admin, RolesEnum::Vendor])){
+          return Inertia::location(route('filament.admin.pages.dashboard'));
+        }
+        else{
+            $route = route('dashboard', absolute: false);
+          }
+
+        return redirect()->intended($route);
     }
 
     /**
